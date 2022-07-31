8b90b2dd0575daa39c32cdc6375e283de4bcd690
juhyeon1114 <juhyeon1114@gmail.com>
2022-07-31 19:34:07 +0900

    toRef, toRefs

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
 <template>
   <h2>Home view</h2>
+  {{ x }}
+  {{ y }}
+  {{ pos }}
+  {{ posAsRefs }}
 </template>
 
-<script>
-export default {};
+<script setup>
+import { reactive, toRef, toRefs } from 'vue';
+
+const pos = reactive({ x: 100, y: 1000 });
+const x = toRef(pos, 'x'); // === ref(100). pos의 x와 동기화 되어 있음.
+const y = toRef(pos, 'y');
+
+const posAsRefs = toRefs(pos);
 </script>
 
 <style></style>
